53adf9612db3a4bb08f150ae2253c8b78552c968
Maciej Borkowski <borkowski.mac@gmail.com>
2020-01-11 07:54:10 +0100

    setup only package without other data

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 setup(
-      name='webscrapermaciej',
+      name='webscraper',
       version=0.1,
-      packages=find_packages(),
+      packages=find_namespace_packages('webscraper', 'webscraper.*'),
       install_requires=('Django>=3.0', 'requests', 'bs4', 'djangorestframework', 'celery', 'redis'),
 )
