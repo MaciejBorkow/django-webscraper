b54aac6047669fbebc36629cfdb9fcc3ba482b94
Maciej Borkowski <borkowski.mac@gmail.com>
2020-01-11 07:30:36 +0100

    change app name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-      name='webscraper',
+      name='webscrapermaciej',
       version=0.1,
       packages=find_packages(),
       install_requires=('Django>=3.0', 'requests', 'bs4', 'djangorestframework', 'celery', 'redis'),
