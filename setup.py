1299a8a31b16ad1a780dd962adddbb4413bb9ec3
Maciej Borkowski <borkowski.mac@gmail.com>
2020-01-11 12:49:26 +0100

    setup packages

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages, find_namespace_packages
 setup(
       name='webscraper',
       version=0.1,
-      packages=find_namespace_packages('webscraper', 'webscraper.*'),
+      author='Maciej Borkowski',
+      author_email='borkowski.mac@gmail.com',
+      python_requires='~=3.7',
       install_requires=('Django>=3.0', 'requests', 'bs4', 'djangorestframework', 'celery', 'redis'),
+      packages=['webscraper'],
 )
